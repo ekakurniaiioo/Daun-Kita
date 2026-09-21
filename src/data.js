03fe79cceab6b3a0373529d2ae5b8acd19f3e90d
Eka Kurnia <kurniae047@gmail.com>
2026-09-21 17:31:28 +0700

    Add static plant data

---
+++ src/data.js
@@ -0,0 +1,58 @@
+export const dummyPlants = [
+  {
+    id: 1,
+    nama: "Monstera Deliciosa",
+    kategori: "Indoor",
+    harga: 85000,
+    stok: 12,
+  },
+  {
+    id: 2,
+    nama: "Snake Plant",
+    kategori: "Indoor",
+    harga: 45000,
+    stok: 20,
+  },
+  {
+    id: 3,
+    nama: "Pothos Golden",
+    kategori: "Gantung",
+    harga: 30000,
+    stok: 15,
+  },
+  {
+    id: 4,
+    nama: "Kaktus Mini",
+    kategori: "kaktus & Sukulen",
+    harga: 15000,
+    stok: 30,
+  },
+  {
+    id: 5,
+    nama: "Aloe Vera",
+    kategori: "Kaktus & Sukulen",
+    harga: 25000,
+    stok: 18,
+  },
+  {
+    id: 6,
+    nama: "Kamboja Jepang",
+    kategori: "Outdoor",
+    harga: 120000,
+    stok: 5,
+  },
+  {
+    id: 7,
+    nama: "Calathea Orbifolia",
+    kategori: "Indoor",
+    harga: 95000,
+    stok: 0,
+  },
+  {
+    id: 8,
+    nama: "String of Pearls",
+    kategori: "Gantung",
+    harga: 55000,
+    stok: 8,
+  },
+];
